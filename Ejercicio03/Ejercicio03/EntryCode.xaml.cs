8d0bbcde75b2e13b331ba4780562a5a1cb7cea85
Gustavo <gblanco@gmail.com>
2021-04-06 22:22:17 -0500

    first commit

---
+++ Ejercicio03/Ejercicio03/EntryCode.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio03
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EntryCode : ContentPage
+    {
+        public EntryCode()
+        {
+            InitializeComponent();
+            StackLayout stack = new StackLayout();
+            var entry = new Entry { Text = "I am an Editor"};
+            stack.Children.Add(entry);
+            Content = stack;
+        }
+    }
+}
